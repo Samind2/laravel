579b32c3aef9cef7352fe6c734bef13ba2cc72e4
Wuttha <Wuttha@webmail.npru.ac.th>
2024-07-12 12:36:16 +0700

    first commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    //Then, define the logic for fetching and displaying posts in the PostController
+    public function index()
+    {
+        $posts = [
+            ['id' => 1, 'title' => 'Laravel 8 is Released!', 'posted_by' => 'Tania Rascia'],
+            ['id' => 2, 'title' => 'The Road to Vue 3: Vite', 'posted_by' => 'Lachlan Miller'],
+            ['id' => 3, 'title' => 'The Road to Vue 3: Vue Router', 'posted_by' => 'Lachlan Miller'],
+            ['id' => 4, 'title' => 'The Road to Vue 3: Vue 3', 'posted_by' => 'Lachlan Miller'],
+        ];
+
+        return view('posts.index', ['posts' => $posts]);
+    }
+}
